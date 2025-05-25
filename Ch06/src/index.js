bc6172d8f06245c1bb86d5d7d4a8fa45e8ef1bcd
X.Adi0251 <166922118+adityagupta0251@users.noreply.github.com>
2025-05-25 14:38:09 +0530

    completed ch06

--- Ch06/src/index.js
+++ Ch06/src/index.js
@@ -1,43 +1,17 @@
 import dotenv from "dotenv";
-// or we can : 
-// require('dotenv').config({ path: './env' })
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './env'
-})
+// Load environment variables from the specified path
+dotenv.config({ path: './env' });
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,  () => {
-        console.log(`Server is running at port : $
-            {process.env.PORT}`)
-    })
-})
-.catch((err) => {
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
     console.error("MONGODB connection failed", err);
-    
-});
-
-// import express from "express";
-
-// const app = express();
-
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        
-//         app.on("error", (error) => {
-//             console.error("ERROR:", error);
-//         });
-
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`Aap is listening on port : ${process.env.PORT}`)
-//         })
-
-//         console.log("Database connected successfully");
-//     } catch (error) {
-//         console.error("ERROR:", error);
-//         throw error;
-//     }
-// })();
+  });
